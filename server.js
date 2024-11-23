ef7e94cc562e266dd2a68f431c967956a141617b
ItzzzAk <kalelamol07@gmail.com>
2024-11-23 19:57:44 +0530

    serverjs changes by ak

--- server.js
+++ server.js
@@ -47,20 +47,25 @@ const server = express();
 server.set("trust proxy", 1);
 const upload = multer({ storage: multer.memoryStorage() });
 // CORS setup
-const corsOptions = {
-  origin: [
-    "https://isrc.org.in",
-    "https://www.isrc.org.in",
-    "http://localhost:3000",
-  ], // Allowed domains
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true, // Required for cookies, Authorization headers with HTTPS
-};
+const allowedOrigins = ["https://isrc.org.in", "https://www.isrc.org.in"];
 
-// Place CORS middleware at the top
-server.use(cors(corsOptions));
-server.options("*", cors(corsOptions)); // Handle preflight
+server.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+// Handle preflight requests globally
+server.options("*", cors());
 
 server.use(express.json()); // Built-in body-parser for JSON
 server.use(express.urlencoded({ extended: true })); // Built-in body-parser for URL-encoded data
